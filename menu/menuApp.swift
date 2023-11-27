f02cf15f829c799dee939df429d71666474a8b69
Omer Cagri Sayir <omercagris@gmail.com>
2023-11-27 20:27:08 +0300

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Omer Cagri Sayir on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
