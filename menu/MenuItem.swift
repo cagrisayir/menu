c70508a502318d9ddb8a9c64e17b9774000b72be
Omer Cagri Sayir <omercagris@gmail.com>
2023-11-28 07:20:33 +0300

    created MenuItem struct

---
+++ menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  menu
+//
+//  Created by Omer Cagri Sayir on 28.11.2023.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
